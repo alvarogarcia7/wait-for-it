f6ce5e0bf65b8ca561bf65dd93857af4f3543912
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2018-03-13 15:54:17 +0100

    Add spike for http requests
    Sample usage
    
    ```#!/bin/sh
    
    set -euxo pipefail
    
    read healthcheck <<HEALTHCHECK
    curl -d '{"firstName": "JOHAN23111", "secondName": "KRYIFF"}' -H 'Content-Type: application/json' -s -o /dev/null -w \"%{http_code}\" http://34.244.254.247/v1/greeting/greet
    HEALTHCHECK
    
    ./healthcheck.sh "$healthcheck" "[ it -eq 200 ]" -s -t 10 -- "$@"
    ```

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 #   Use this script to test if a given TCP host/port are available
+
+set -x
 
 cmdname=$(basename $0)
 
@@ -9,10 +11,7 @@ usage()
 {
     cat << USAGE >&2
 Usage:
-    $cmdname host:port [-s] [-t timeout] [-- command args]
-    -h HOST | --host=HOST       Host or IP under test
-    -p PORT | --port=PORT       TCP port under test
-                                Alternatively, you specify the host and port as host:port
+    $cmdname "predicate" [-s] [-t timeout] [-- command args]
     -s | --strict               Only execute subcommand if the test succeeds
     -q | --quiet                Don't output any status messages
     -t TIMEOUT | --timeout=TIMEOUT
@@ -25,23 +24,27 @@ USAGE
 wait_for()
 {
     if [[ $TIMEOUT -gt 0 ]]; then
-        echoerr "$cmdname: waiting $TIMEOUT seconds for $HOST:$PORT"
+        echoerr "$cmdname: waiting $TIMEOUT seconds for the healthcheck"
     else
-        echoerr "$cmdname: waiting for $HOST:$PORT without a timeout"
+        echoerr "$cmdname: waiting for the healthcheck without a timeout"
     fi
     start_ts=$(date +%s)
     while :
     do
-        if [[ $ISBUSY -eq 1 ]]; then
-            nc -z $HOST $PORT
-            result=$?
-        else
-            (echo > /dev/tcp/$HOST/$PORT) >/dev/null 2>&1
-            result=$?
-        fi
+		service_response=$(eval $healthcheck)
+		interpolated_predicate=${predicate/it/$service_response} # the healthcheck is a string containing a predicate. Must be interpreted to replace 'it' for the healthcheck result.
+		$(eval $interpolated_predicate)
+		is_service_running=$?
+		if [ $is_service_running -eq 0 ]; then
+			echo "Service OK"
+			result=0
+		else 
+			echo "Service KO"
+			result=1
+		fi
         if [[ $result -eq 0 ]]; then
             end_ts=$(date +%s)
-            echoerr "$cmdname: $HOST:$PORT is available after $((end_ts - start_ts)) seconds"
+            echoerr "$cmdname: the healthcheck is available after $((end_ts - start_ts)) seconds"
             break
         fi
         sleep 1
@@ -53,30 +56,29 @@ wait_for_wrapper()
 {
     # In order to support SIGINT during timeout: http://unix.stackexchange.com/a/57692
     if [[ $QUIET -eq 1 ]]; then
-        timeout $BUSYTIMEFLAG $TIMEOUT $0 --quiet --child --host=$HOST --port=$PORT --timeout=$TIMEOUT &
+        timeout $BUSYTIMEFLAG $TIMEOUT $0 --quiet --child --timeout=$TIMEOUT &
     else
-        timeout $BUSYTIMEFLAG $TIMEOUT $0 --child --host=$HOST --port=$PORT --timeout=$TIMEOUT &
+        timeout $BUSYTIMEFLAG $TIMEOUT $0 "$healthcheck" "$predicate" --child --timeout=$TIMEOUT &
     fi
     PID=$!
     trap "kill -INT -$PID" INT
     wait $PID
     RESULT=$?
     if [[ $RESULT -ne 0 ]]; then
-        echoerr "$cmdname: timeout occurred after waiting $TIMEOUT seconds for $HOST:$PORT"
+        echoerr "$cmdname: timeout occurred after waiting $TIMEOUT seconds for the healthcheck to succeed"
     fi
     return $RESULT
 }
 
 # process arguments
+healthcheck="$1"
+shift 1
+predicate="$1"
+shift 1
+
 while [[ $# -gt 0 ]]
 do
     case "$1" in
-        *:* )
-        hostport=(${1//:/ })
-        HOST=${hostport[0]}
-        PORT=${hostport[1]}
-        shift 1
-        ;;
         --child)
         CHILD=1
         shift 1
@@ -87,24 +89,6 @@ do
         ;;
         -s | --strict)
         STRICT=1
-        shift 1
-        ;;
-        -h)
-        HOST="$2"
-        if [[ $HOST == "" ]]; then break; fi
-        shift 2
-        ;;
-        --host=*)
-        HOST="${1#*=}"
-        shift 1
-        ;;
-        -p)
-        PORT="$2"
-        if [[ $PORT == "" ]]; then break; fi
-        shift 2
-        ;;
-        --port=*)
-        PORT="${1#*=}"
         shift 1
         ;;
         -t)
@@ -118,7 +102,7 @@ do
         ;;
         --)
         shift
-        CLI=("$@")
+        CLI="$@"
         break
         ;;
         --help)
@@ -130,11 +114,6 @@ do
         ;;
     esac
 done
-
-if [[ "$HOST" == "" || "$PORT" == "" ]]; then
-    echoerr "Error: you need to provide a host and port to test."
-    usage
-fi
 
 TIMEOUT=${TIMEOUT:-15}
 STRICT=${STRICT:-0}
